b03378edb08719d7997c92601bb5df580d123b6d
Randy <randy.joseph.avalos@gmail.com>
2020-01-28 21:32:30 -0800

    Add middle rt load and transform function

--- scratch.R
+++ scratch.R
@@ -88,18 +88,77 @@ transform_real_time <- function(read_path = "./data/rt_lmp/",
   
 }
 
-transform_real_time()
-
-
-
-test <- as.data.frame(list.files(path = "./data/rt_lmp/", pattern = ".csv"))
+test <- transform_real_time()
 test
 
-colnames(test) <-  c("filename")
-test <- test %>%
-  separate(filename, into = c("date", "file_type"), sep = 8, remove = FALSE)
 
-test
+# test <- as.data.frame(list.files(path = "./data/rt_lmp/", pattern = ".csv"))
+# test
+# 
+# colnames(test) <-  c("filename")
+# test <- test %>%
+#   separate(filename, into = c("date", "file_type"), sep = 8, remove = FALSE)
+# 
+# test
+test$filename
+lapply(test$filename, transform_real_time_load_transform, write_directory =  "data/rt_lmp/transformed/")
+
+transform_real_time_load_transform <- function(filename,
+                                               write_directory){
+  transformed_df <- read_csv(paste0("./data/rt_lmp/", filename),
+                               skip = 8,
+                               col_names = TRUE,
+                               cols(
+                                 Node = col_character(),
+                                 Type = col_character(),
+                                 Value = col_character(),
+                                 `HE 1` = col_double(),
+                                 `HE 2` = col_double(),
+                                 `HE 3` = col_double(),
+                                 `HE 4` = col_double(),
+                                 `HE 5` = col_double(),
+                                 `HE 6` = col_double(),
+                                 `HE 7` = col_double(),
+                                 `HE 8` = col_double(),
+                                 `HE 9` = col_double(),
+                                 `HE 10` = col_double(),
+                                 `HE 11` = col_double(),
+                                 `HE 12` = col_double(),
+                                 `HE 13` = col_double(),
+                                 `HE 14` = col_double(),
+                                 `HE 15` = col_double(),
+                                 `HE 16` = col_double(),
+                                 `HE 17` = col_double(),
+                                 `HE 18` = col_double(),
+                                 `HE 19` = col_double(),
+                                 `HE 20` = col_double(),
+                                 `HE 21` = col_double(),
+                                 `HE 22` = col_double(),
+                                 `HE 23` = col_double(),
+                                 `HE 24` = col_double())
+  ) %>%
+    mutate(date = test$date[1],
+           data_transform_date = Sys.Date()
+    ) %>%
+    gather(key = hour_text, value = price, `HE 1`:`HE 24`) %>%
+    separate(hour_text, into = c("test", "hour_numeric"), sep = " ", remove = FALSE) %>%
+    mutate(temp = mapply(function(x, y) paste0(rep(x, y), collapse = ""), 0, 2 - nchar(hour_numeric))) %>%
+    mutate(hour_numeric = as.numeric(paste0(temp, hour_numeric))) %>%
+    mutate(datetime = ymd_hm(paste0(date, " ", hour_numeric, ":00"))) %>%
+    arrange(Node, date, hour_numeric) %>%
+    mutate(rt_source = "rt_lmp_final") %>%
+    mutate(date = ymd(date)) %>%
+    select(Node, Type, Value, date, datetime, rt_price = price, hour_text, hour_numeric, rt_source, data_transform_date)
+  
+  write_csv(transformed_df, path=paste0(write_directory, filename), Encoding('UTF-8')) # write transformed file
+  #print(paste0(filename, " transformed and saved to ", write_directory))
+  
+}
+
+
+
+
+
 
 transformed_test <- read_csv(paste0("./data/rt_lmp/", test$filename[1]),
                  skip = 8,
